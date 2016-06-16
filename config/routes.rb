e3f4fabc37d1279cbd4d332f706c2ffa8c6aba07
kwebster2 <kevin.r.webster@gmail.com>
2016-06-16 14:53:39 -0400

    successfully producing JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :pickups
-  resources :listings
-  resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :pickups
+      resources :listings
+      resources :donors
+      resources :recipients
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
