0edb88b86b8b5daff8bc6b1d40ba2480c6431cf9
kwebster2 <kevin.r.webster@gmail.com>
2016-06-17 10:47:10 -0400

    added has_secure_password to user model

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'rack-cors'
 
 gem 'rails-api'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-donor = User.create(email: "bob@bob.com", name: "Bob's Burgers", address: "Ocean Avenue", description: "Meh", phone_number: "999-999-9999", type: "Donor")
+donor = User.create(email: "bob@bob.com", name: "Bob's Burgers", address: "Ocean Avenue", description: "Meh", phone_number: "999-999-9999", type: "Donor", password: "password")
 
-donor2 = User.create(email: "rob@rob.com", name: "Rob's Burgers", address: "Seaside Avenue", description: "Bleh", phone_number: "777-777-7777", type: "Donor")
+donor2 = User.create(email: "rob@rob.com", name: "Rob's Burgers", address: "Seaside Avenue", description: "Bleh", phone_number: "777-777-7777", type: "Donor", password: "password")
 
-recipient = User.create(email: "archer@isis.com", name: "Archer", description: "Hate the KGB", address: "NYC", phone_number: "666-666-6666", type: "Recipient")
+recipient = User.create(email: "archer@isis.com", name: "Archer", description: "Hate the KGB", address: "NYC", phone_number: "666-666-6666", type: "Recipient", password: "password")
 
-recipient2 = User.create(email: "mother@isis.com", name: "Mother", description: "Hate my son", address: "ISIS", phone_number: "123-123-1233", type: "Recipient")
+recipient2 = User.create(email: "mother@isis.com", name: "Mother", description: "Hate my son", address: "ISIS", phone_number: "123-123-1233", type: "Recipient", password: "password")
 
 donor.listings << Listing.create(pickup_time: "21:00", description: "Pickup my food")
 
