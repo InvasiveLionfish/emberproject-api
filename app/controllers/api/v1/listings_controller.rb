ccc72103461fb6f31f04405284f0580d13f5699e
kwebster2 <kevin.r.webster@gmail.com>
2016-06-17 15:52:45 -0400

    removed authentication from listings controller index action

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class ListingsController < ApplicationController
-      skip_before_action :authenticate
 
       def index
         render json: Listing.includes(:donor), include: ['donor']
