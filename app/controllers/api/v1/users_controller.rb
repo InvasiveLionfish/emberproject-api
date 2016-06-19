9e9e0196e2c5940854623b0c8d4fefef4a38bfa5
kwebster2 <kevin.r.webster@gmail.com>
2016-06-19 12:54:48 -0400

    can create new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,24 @@
 module Api
   module V1
-class UsersController < ApplicationController
+    class UsersController < ApplicationController
+    skip_before_action :authenticate, only: [:create]
+    skip_before_filter :verify_authenticity_token
 
-  def create
-    binding.pry
-    @user = User.new
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: {}, status: 200
+        else
+          render json: ErrorSerializer.serialize(user.errors), status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:name, :email, :password, :description, :address, "phone-number", :type])
+      end
+
+    end
   end
 end
-
-end
-end 
