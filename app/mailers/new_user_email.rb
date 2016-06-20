a82a94c41d4a15f7f400f4cc6bd3035cb62fc0b0
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-20 17:44:39 -0400

    ActionMailer working

--- app/mailers/new_user_email.rb
+++ app/mailers/new_user_email.rb
@@ -1,6 +1,6 @@
 class NewUserEmail < ApplicationMailer
   def notify_user(user)
     @user = user
-    mail(to: @user.email, subject: "Welcome to Fair/Fair")
+    mail(to: @user.email, subject: "Welcome to Fair/Fare")
   end
 end
