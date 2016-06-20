fd62b08b157f462b2094de7a765a66d581275973
kwebster2 <kevin.r.webster@gmail.com>
2016-06-20 02:20:04 -0400

    form works

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -5,6 +5,22 @@ module Api
       def index
         render json: Listing.includes(:donor), include: ['donor']
       end
+
+      def create
+        binding.pry
+        listing = Listing.new(listing_params)
+        if listing.save
+          current_user.listings << listing
+          render json: listing
+        else
+          render json: ErrorSerializer.serialize(listing.errors), status: 422
+        end
+      end
+
+      private
+        def listing_params
+          ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:description, "pickup-time"])
+        end
     end
   end
 end
