ff0aacb69e29470276a369c93d17da2692830e1b
kwebster2 <kevin.r.webster@gmail.com>
2016-06-21 06:35:39 -0400

    added recipient relationship to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
   belongs_to :donor
   has_one :pickup
+  has_one :recipient, through: :pickup
 end
--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -2,4 +2,5 @@ class ListingSerializer < ActiveModel::Serializer
   attributes :id, :pickup_time, :description
   belongs_to :donor
   has_one :pickup
+  has_one :recipient, through: :pickup
 end
